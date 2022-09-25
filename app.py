bc0615b8f4b7a415a9216bd8cb28c167ef8b394d
Anirban Saha <mailme@anirbansaha.com>
2022-09-25 21:07:42 +0530

    ++
    +

--- app.py
+++ app.py
@@ -259,6 +259,8 @@ def getBirds():
     response['message'].append("Replaced Emojis in the tweet: "+tweet) 
     tweet = try_replacing_hashtags_mit_birdname(tweet,all_birds_list, birdnames_words)
     response['message'].append("Replaced Hashtag: "+tweet) 
+    tweet = basic_preprocess(tweet, spelling_corrections)
+    response['message'].append("Basic Preprocessing done: "+tweet) 
   except Exception as e:
     response['error'].append("Failed getting the sentence. Sorry.") 
     response['error'].append("Adding demo sentence: There is a blue throated barbet on my window.")
