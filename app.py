32dc06210c47c81c1e80e7639ae49a50db98ece5
Anirban Saha <mailme@anirbansaha.com>
2022-09-25 09:59:18 +0530

    x
    x

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ app = Flask(__name__)
 def hello_world():
   return "Hello World"
 
-port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, port = port) 
+
+if __name__ == '__main__':
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port = port) 
+    #app.run()  # If address is in use, may need to terminate other sessions:
+               # Runtime > Manage Sessions > Terminate Other Sessions
+      
+
