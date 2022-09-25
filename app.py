f840744de91dab8b8e2f7086fb8d95a2a1a43ed0
Anirban Saha <mailme@anirbansaha.com>
2022-09-25 09:49:23 +0530

    app
    app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
   return "Hello World"
 
 
-if __name__ == '__main__':
+if __name__ == '__app__':
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, port = port)
