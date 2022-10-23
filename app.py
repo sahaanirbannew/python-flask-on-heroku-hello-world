396ffc1b12b24febd813ef5e0009957c58ef03f4
Anirban Saha <mailme@anirbansaha.com>
2022-10-23 10:59:07 +0530

    -
    -

--- app.py
+++ app.py
@@ -368,7 +368,3 @@ if __name__ == '__main__':
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, port = port) 
-    #app.run()  # If address is in use, may need to terminate other sessions:
-               # Runtime > Manage Sessions > Terminate Other Sessions
-      
-
